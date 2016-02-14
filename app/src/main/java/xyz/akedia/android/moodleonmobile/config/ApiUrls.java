9aaeba5760e869e37858914b151ec904d3b7d502
Abhishek Kedia <kedia.abhishek10@gmail.com>
2016-02-14 23:06:07 +0530

    Create model and api class

---
+++ app/src/main/java/xyz/akedia/android/moodleonmobile/config/ApiUrls.java
@@ -0,0 +1,7 @@
+package xyz.akedia.android.moodleonmobile.config;
+
+/**
+ * Contains urls of apis
+ */
+public class ApiUrls {
+}
---
+++ app/src/main/java/xyz/akedia/android/moodleonmobile/model/User.java
@@ -0,0 +1,9 @@
+package xyz.akedia.android.moodleonmobile.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by akedia on 14/02/16.
+ */
+public class User implements Serializable {
+}
