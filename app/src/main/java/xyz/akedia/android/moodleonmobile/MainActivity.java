01477f265ce2a9197bd1cffef5e7f1c4a2745b51
Abhishek Kedia <kedia.abhishek10@gmail.com>
2016-02-22 23:36:15 +0530

    Rename MainActivity to StartActivity for merging

--- app/src/main/java/xyz/akedia/android/moodleonmobile/MainActivity.java
+++ app/src/main/java/xyz/akedia/android/moodleonmobile/MainActivity.java
@@ -1,71 +1,7 @@
 package xyz.akedia.android.moodleonmobile;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.util.Log;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.widget.Toast;
-
-import xyz.akedia.android.moodleonmobile.app.MoodleOnMobile;
-
-public class MainActivity extends AppCompatActivity {
-
-    public void continueToLogin(View view){
-        Intent i = new Intent(this,LoginActivity.class);
-        startActivity(i);
-//        MainActivity.this.finish();
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-                Snackbar.make(view, String.format("Moodle Url = %s",((MoodleOnMobile) getApplication()).getMoodleUrl()), Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-//        PreferenceManager.setDefaultValues(this, R.xml.preferences, false); //Don't think this is needed. TODO test without this
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            Log.d("-->", "clicked settings");
-            Intent i = new Intent(this,SettingsActivity.class);
-            startActivity(i);
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+/**
+ * Created by akedia on 22/02/16.
+ */
+public class MainActivity {
 }
