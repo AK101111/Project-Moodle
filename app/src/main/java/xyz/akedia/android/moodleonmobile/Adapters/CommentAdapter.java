d72671237a27aaa1442be73837a1bde27eb04579
Ashish Ranjan <ashishranjan2912@gmail.com>
2016-02-24 04:00:37 +0530

    add thread details layout

---
+++ app/src/main/java/xyz/akedia/android/moodleonmobile/Adapters/CommentAdapter.java
@@ -0,0 +1,7 @@
+package xyz.akedia.android.moodleonmobile.Adapters;
+
+/**
+ * Created by ashish on 24/2/16.
+ */
+public class CommentAdapter {
+}
---
+++ app/src/main/java/xyz/akedia/android/moodleonmobile/Comment.java
@@ -0,0 +1,7 @@
+package xyz.akedia.android.moodleonmobile;
+
+/**
+ * Created by ashish on 24/2/16.
+ */
+public class Comment {
+}
